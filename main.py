e494533a4af9221dcfe3715f5a9b48d8a7da2363
Saul Garcia Ramos <saul.garcia4654@alumnos.udg.mx>
2021-09-01 13:27:19 -0500

    Prueba de repositorio

---
+++ main.py
@@ -0,0 +1,8 @@
+#Garcia Ramos Saul Ezequiel
+#Codigo: 215465492
+#Analizador Lexico Completo
+#Seminario de Solucion de problemas de Traductores 2
+#Profesor: Michel Emanuel
+
+
+print("Hola Mundo Prueba")
